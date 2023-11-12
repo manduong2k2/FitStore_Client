dae33ad93fc194935ccd7621139f738cd6046524
trnvinh1107 <vinhtranthe117@gmail.com>
2023-11-12 18:33:25 +0700

    add success

---
+++ index.js
@@ -0,0 +1,19 @@
+const morgan = require("morgan");
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+//http logger
+app.use(morgan('combined'));
+
+//template engine
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.get("/", (req, res) => {
+  res.render("home", {titlePage: 'home'});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
