7c06393ebc228158953d9c1741b2001cf45fef2f
Khac Loc <locn1504@gmail.com>
2023-11-15 15:10:03 +0700

    them dang ky, dang nhap

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
-  res.render("home", { titlePage: "home" });
+  res.render("home", { titlePage: "Trang chủ" });
 });
 app.get("/introduce", (req, res) => {
-  res.render("introduce", { titlePage: "introduce" });
+  res.render("introduce", { titlePage: "Giới thiệu" });
+});
+app.get("/signin", (req, res) => {
+  res.render("signin", { titlePage: "Đăng nhập" });
+});
+app.get("/signup", (req, res) => {
+  res.render("signup", { titlePage: "Đăng ký" });
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
