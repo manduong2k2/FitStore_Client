9c8067cdb854bcef31547725519ec9d661ee11af
manduong2k2 <manduong2k2@gmail.com>
2023-11-19 09:55:04 +0700

    ưerew

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 router.use(express.json());
+const axios = require('axios');
+const cookieParser = require('cookie-parser');
+router.use(express.urlencoded({ extended: true }));
 
 router.get("/", (req, res) => {
   res.render("home", { titlePage: "Trang chủ" });
@@ -16,5 +19,48 @@ router.get("/signup", (req, res) => {
 });
 router.get("/signin", (req, res) => {
   res.render("signin", { titlePage: "Đăng nhập" });
+})
+//
+router.use(cookieParser());
+//
+router.post('/login', (req, res) => {
+  // Gửi yêu cầu đăng nhập tới server
+  axios.post('http://jul2nd.ddns.net/account/login', {
+    username: req.body.email,
+    password: req.body.password
+  })
+  .then(response => {
+    // Lưu session ID vào cookie
+    var account = JSON.parse(response.data.account);
+    console.log(account);
+    res.cookie('id', account.id );
+    res.cookie('username', account.username);
+    res.cookie('name', account.name);
+    res.cookie('image', account.image);
+    res.cookie('email', account.email);
+    res.cookie('sessionId', response.data.sessionId);
+    res.redirect('/');
+  })
+  .catch(error => {
+    res.send(error);
+  });
+});
+router.get('/logout', (req, res) => {
+  // Xóa cookie có tên 'sessionId'
+  res.clearCookie('sessionId', { SameSite: 'None', httpOnly: false, secure: true });
+  res.clearCookie('id', { SameSite: 'None', httpOnly: false, secure: true });
+  res.clearCookie('username', { SameSite: 'None', httpOnly: false, secure: true });
+  res.clearCookie('name', { SameSite: 'None', httpOnly: false, secure: true });
+  res.clearCookie('image', { SameSite: 'None', httpOnly: false, secure: true });
+  res.clearCookie('email', { SameSite: 'None', httpOnly: false, secure: true });
+  res.redirect('/');
+});
+router.get('/protected', async (req, res) => {
+    try {
+      const response = await axios.get('http://jul2nd.ddns.net/account/protected', { withCredentials: true });
+      res.json(response.data);
+    } catch (error) {
+      res.status(error.response.status).json({ message: error.response.data.message });
+    }
 });
 module.exports = router;
--- index.js
+++ index.js
@@ -5,29 +5,17 @@ const brandController = require('./controller/brand.controller');
 const app = express();
 const port = 3000;
 const path = require("path");
-const request = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-const requestOptions = {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-  },
-};
+
 
 //http logger
 app.use(morgan("combined"));
 app.use('', homeController);
 app.use('/brand', brandController);
 //template engine
-app.use('public',express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
+//
 app.listen(port, async () => {
-  const response = await request("http://jul2nd.ddns.net/product");
-  if (response.status === 200) {
-    const data = await response.json();
-    console.log(data);
-  }
   console.log(`Example app listening on port ${port}`);
 });
