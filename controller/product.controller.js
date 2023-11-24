e6fdcc915ca5f6d1d627f4ffb73fb197e4025d9e
manduong2k2 <manduong2k2@gmail.com>
2023-11-24 21:43:59 +0700

    detail product

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 router.use(express.json());
-const cookieParser = require('cookie-parser');
 var LocalStorage = require('node-localstorage').LocalStorage;
 localStorage = new LocalStorage('./scratch'); 
+const request = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 router.use(express.urlencoded({ extended: true }));
-const request = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
 
 router.get("/create", (req, res) => {
   res.render("product/createProduct", { titlePage: "Thêm sản phẩm" });
@@ -20,5 +19,20 @@ router.get("/list", (req, res) => {
       res.render("product/listProduct", { titlePage: "Danh sách sản phẩm", data: data });
     }
   });
+});
+router.get("/details/:id", async (req, res) => {
+  try{
+    const response = await request('http://jul2nd.ddns.net/product/'+req.params.id,{
+      method: 'GET',
+      withCredentials: true,
+      headers: {
+        Authorization: localStorage.getItem('token')
+      }
+    });
+    const data = await response.json();
+    res.render("product/detailProduct", { titlePage: "Chi tiết sản phẩm" , product: data.product , solds: data.solds});
+  }catch(err){
+    res.send(err);
+  }
 });
 module.exports = router;
